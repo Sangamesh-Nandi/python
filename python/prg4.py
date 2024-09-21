b034a88252cad6513d9402f636370a2b2c1baa36
Sangamesh Nandi <sangameshv.24.beai@acharya.ac.in>
2024-09-21 23:17:19 +0530

    Add files via upload

---
+++ python/prg4.py
@@ -0,0 +1,8 @@
+def multiply(a, b):
+    return a * b
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = multiply(num1, num2)
+print(f"The product of {num1} and {num2} is {result}")
